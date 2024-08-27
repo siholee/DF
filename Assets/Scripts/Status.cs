5ee37011db79399a9400feecf60444447d23e074
siholee <siholee99@gmail.com>
2024-08-27 22:28:38 +0900

    0827
    스테이터스 관리 및 업데이트 추가

---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    public string NAME;
+    public string TRAIT;
+    public string ATTRIBUTE;
+    public float MULBUFF;
+    public float SUMBUFF;
+    public int REMAIN_COUNT;
+    public int REMAIN_TURN;
+    public string CUSTOM_EFFECT;
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,14 +7,43 @@ public class Unit : MonoBehaviour
     public string TYPE;
     public string RANK;
     public int HP;
+    public int HP_MAX;
     public int ATK;
     public int DEF;
     public int ATK_RANGE;
     public double ATK_SPD;
     public double MOVE_SPD;
+    public float CRT_POS;
+    public float CRT_DMG;
 
-    public int HP_CURRENT;
-    public bool ATTACHED;
+    public int HP_BASE;
+    public float HP_MULBUFF;
+    public float HP_SUMBUFF;
+
+    public int ATK_BASE;
+    public float ATK_MULBUFF;
+    public float ATK_SUMBUFF;
+
+    public int DEF_BASE;
+    public float DEF_MULBUFF;
+    public float DEF_SUMBUFF;
+
+    public int ATK_SPD_BASE;
+    public float ATK_SPD_MULBUFF;
+    public float ATK_SPD_SUMBUFF;
+
+    public int MOVE_SPD_BASE;
+    public float MOVE_SPD_MULBUFF;
+    public float MOVE_SPD_SUMBUFF;
+
+    public float CRT_POS_BASE;
+    public float CRT_POS_BUFF;
+    
+    public float CRT_DMG_BASE;
+    public float CRT_DMG_BUFF;
+
+    public List<Status> STATUS_MANAGER;
+
     public List<GameObject> TARGET_HOLDER;
 
     // Start 메서드에서 원을 생성
@@ -35,5 +64,16 @@ public class Unit : MonoBehaviour
 
         MeshRenderer renderer = rangeIndicator.AddComponent<MeshRenderer>();
         renderer.enabled = false;
+    }
+
+    public void StatusUpdate()
+    {
+        HP_MAX = (int)(HP_BASE * (1 + (HP_MULBUFF * 0.01f)) + HP_SUMBUFF);
+        ATK = (int)(ATK_BASE * (1 + (ATK_MULBUFF * 0.01f)) + ATK_SUMBUFF);
+        DEF = (int)(DEF_BASE * (1 + (DEF_MULBUFF * 0.01f)) + DEF_SUMBUFF);
+        ATK_SPD = (int)(ATK_SPD_BASE * (1 + (ATK_SPD_MULBUFF * 0.01f)) + ATK_SPD_SUMBUFF);
+        MOVE_SPD = (int)(MOVE_SPD_BASE * (1 + (MOVE_SPD_MULBUFF * 0.01f)) + MOVE_SPD_SUMBUFF);
+        CRT_POS = CRT_POS_BASE + CRT_POS_BUFF;
+        CRT_DMG = CRT_DMG_BASE + CRT_DMG_BUFF;
     }
 }
