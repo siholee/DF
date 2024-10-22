e66fb05dea18d1204503476d0f012601452e1660
siholee <35533754+siholee@users.noreply.github.com>
2024-10-22 19:52:36 +0900

    20241022

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    public int SKILL_ID;
+    public string SKILL_NAME;
+    public float SKILL_CT;
+    public float SKILL_CT_MAX;
+}
